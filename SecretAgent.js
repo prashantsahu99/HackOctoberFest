552e72a8b0c682f48a99218eda3315e32f4e03ba
himanshu12334 <111832429+himanshu12334@users.noreply.github.com>
2022-10-22 12:36:02 +0530

    SecretAgent.js

---
+++ SecretAgent.js
@@ -0,0 +1,29 @@
+#content {
+ text-align: center;
+ padding-top: 25%;
+}
+
+body {
+ background:
+  url(https://images.unsplash.com/photo-1529610453335-db84df315b29?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=1336a860de7a33d2f9b4281a13a48b68&auto=format&fit=crop&w=400&q=60);
+ background-size:cover;
+ background-position:center;
+ background-repeat:no-repeat;
+}
+#tc1{
+ color:white;
+ font-weight:700;
+ font-size:5em;
+}
+#tc{
+ color:white;
+}
+html{
+ height:100%;
+}
+hr{
+ width:500px;
+ border-bottom:10px solid blue;
+ border-top:10px solid red;
+}
+<i class="fas fa-user-plus"></i>
